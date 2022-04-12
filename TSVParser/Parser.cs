d98f346edb81bc35f240e809dc0e2ec1de16b24a
joshman1019 <joshman1019@gmail.com>
2022-04-12 15:22:36 -0400

    first commit

---
+++ TSVParser/Parser.cs
@@ -0,0 +1,67 @@
+using System.Data;
+using System.Collections.Generic; 
+using System.IO; 
+
+namespace TSVParser
+{
+    public static class Parser
+    {
+        public static DataTable CreateDataTableFromFile(string path)
+        {
+            DataTable table = new DataTable();
+            var reader = new StreamReader(path);
+            List<string[]> resultsArray = new List<string[]>();
+            string[]? headerRow;
+            int columnCount = 0;
+
+            // if the reader is not null, proceed to split lines on tab delimiter
+            if (reader is not null)
+            {
+                while (!reader.EndOfStream)
+                {
+#pragma warning disable CS8602
+                    resultsArray.Add(reader.ReadLine().Split('\t'));
+#pragma warning restore CS8602
+                }
+            }
+
+            // If the results array count is not zero then we determine column count and 
+            // assign a header row
+            if (resultsArray.Count > 0)
+            {
+                // Assign a column count
+                columnCount = resultsArray[0].Length;
+
+                // Create the header row
+                headerRow = resultsArray[0];
+
+                // Remove the header from the results list
+                resultsArray.RemoveAt(0);
+            }
+            // Otherwise, if count is 0 or less, create an empty header row
+            else
+            {
+                headerRow = new string[] { };
+            }
+
+            // For each string in the header row, create a data column
+            for (int i = 0; i < headerRow.Length; i++)
+            {
+                // Create the columm and name it
+                DataColumn dc = new DataColumn(headerRow[i].ToString());
+
+                // Add teh column to the table
+                table.Columns.Add(dc); 
+            }
+
+            // Now that headers are in palce, add the data to the table
+            foreach (string[] item in resultsArray)
+            {
+                table.Rows.Add(item); 
+            }
+
+            // Finally, return the completed table
+            return table; 
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,30 @@
+﻿using TSVParser; 
+using System.Data; 
+
+namespace Testing
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string somePath = @"C:\Users\c44jhs\Downloads\title.episode.tsv\data.tsv"; 
+            var result = Parser.CreateDataTableFromFile(somePath); 
+            foreach (var rowName in result.Columns)
+            {
+                Console.Write(rowName);
+                Console.Write('\t');
+                Console.WriteLine(); 
+            }
+            foreach (DataRow item in result.Rows)
+            {
+                for (int i = 0; i < result.Columns.Count; i++)
+                {
+                    Console.Write(item[i]);
+                    Console.Write('\t'); 
+                }
+                Console.WriteLine(); 
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
